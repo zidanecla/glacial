c709d3456d12f5fb65d821aa734b737bc45ef396
zidanecla <clane_94@hotmail.com>
2016-03-08 15:20:49 -0600

    frm_Clientes desarrollo 10%

---
+++ Proyecto Glacial/Proyecto Glacial/frm_ClientesMenu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Glacial
+{
+    public partial class frm_ClientesMenu : Form
+    {
+        public frm_ClientesMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
