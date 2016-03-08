c6076b45861a6b874a06000d03f2b47ffb81242a
zidanecla <clane_94@hotmail.com>
2016-03-08 16:32:01 -0600

    frm_ClientesIconoAgregar

--- Proyecto Glacial/Proyecto Glacial/frm_MenuPrincipal.Designer.cs
+++ Proyecto Glacial/Proyecto Glacial/frm_MenuPrincipal.Designer.cs
@@ -28,24 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(260, 93);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
             // 
             // frm_MenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(595, 354);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(884, 661);
             this.Name = "frm_MenuPrincipal";
             this.Text = "Menu Principal";
             this.ResumeLayout(false);
@@ -53,8 +42,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
 
